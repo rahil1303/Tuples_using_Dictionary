36ddceed7ed8a26762f1da751973106c2ae3305b
RAHIL SHARMA <67217341+rahil1303@users.noreply.github.com>
2021-09-25 16:04:32 +0530

    Create 2_In_Memory.py

---
+++ 2_In_Memory.py
@@ -0,0 +1,11 @@
+### Tuples are extremely efficient with regards to time and space management. it is very optimized and is performace is very efficient.
+### Accessing a Tuple
+newTuple = ("a","b","c","d","e","f")
+print(newTuple[1])
+
+print(newTuple[-1])
+
+### Using Slice Operator
+print(newTuple[1:3])
+
+print(newTuple[:])
