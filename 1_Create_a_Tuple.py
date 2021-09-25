41eb9e950cc10c549010fee76190ef7e5fbdcf36
RAHIL SHARMA <67217341+rahil1303@users.noreply.github.com>
2021-09-25 13:44:45 +0530

    Create 1_Create_a_Tuple.py

---
+++ 1_Create_a_Tuple.py
@@ -0,0 +1,7 @@
+mytuple = "1","2","3","4","5"
+mytuple1 = ("1","2","3","4","5")
+mytuple2 = tuple(12345)
+
+#### We can also create a tuple using tuple keyword
+newTuple2 = tuple("abcde") 
+print(newTuple2)
